e897e4f081f1143a179a8523c3caf5aa7dccc821
Boo-coder <Bouna112@yahoo.com>
2024-12-12 23:38:16 -0500

    db

---
+++ src/00-objectball.js
@@ -0,0 +1,125 @@
+const gameObject = {
+  "home": {
+    "teamName": "brooklyn Nets",
+    "colors": ["Black, Whire"],
+    "players": {
+      "Alan Anderson": { 
+          "number": 0,
+          "shoe": 16, 
+          "Points": 22, 
+          "Rebounds": 12, 
+          "Assist": 12, 
+          "Steals": 3, 
+          "Blocks": 1, 
+          "SlamDunks": 1,   
+      },
+
+      "Reggie Evans": {
+          "number": 30,
+          "shoe": 14, 
+          "Points": 12, 
+          "Rebounds": 12, 
+          "Assist": 12, 
+          "Steals": 12, 
+          "Blocks": 12, 
+          "SlamDunks": 7,
+      },
+
+      "Brook Lopez": {
+          "number": 11,
+          "shoe": 17, 
+          "Points": 17, 
+          "Rebounds": 19, 
+          "Assist": 10, 
+          "Steals": 3, 
+          "Blocks": 1, 
+          "SlamDunks": 15,
+      },
+
+      "Mason Plumlee": {
+          "number": 1,
+          "shoe": 19, 
+          "Points": 26, 
+          "Rebounds": 12, 
+          "Assist": 6, 
+          "Steals": 3, 
+          "Blocks": 8, 
+          "SlamDunks": 5,
+      },
+
+      "Jason Terry": {
+          "number": 31,
+          "shoe": 15, 
+          "Points": 19, 
+          "Rebounds": 2, 
+          "Assist": 2, 
+          "Steals": 4, 
+          "Blocks": 11, 
+          "SlamDunks": 1,
+      },
+    }
+},
+
+ "away": {
+  "teamName": "Charlotte Hornets",
+  "colors": ["Turquoise", "Purple"],
+  "players": {
+    "Jeff Adrien": {
+      "number": 4,
+      "shoe": 18,
+      "Points": 10,
+      "Rebounds": 1,
+      "Assist": 1,
+      "Steals": 1,
+      "Blocks": 7,
+      "SlamDunks": 2,
+    },
+
+    "Bismak Biyombo": {
+      "number": 0,
+      "shoe": 16,
+      "Points": 12,
+      "Rebounds": 4,
+      "Assist": 7,
+      "Steals": 7,
+      "Blocks": 15,
+      "SlamDunks": 10,
+    },
+
+    "DeSagna Diop": {
+      "number": 2,
+      "shoe": 14,
+      "Points": 24,
+      "Rebounds": 12,
+      "Assist": 12,
+      "Steals": 4,
+      "Blocks": 5,
+      "SlamDunks": 5,
+    },
+
+    "Bem Gordon": {
+      "number": 8,
+      "shoe": 15,
+      "Points": 33,
+      "Rebounds": 3,
+      "assists": 2,
+      "Steals": 1,
+      "Blocks": 1,
+      "SlamDunks": 0,
+    },
+
+    "Brendan Haywood": {
+      "number": 33,
+      "shoe": 15,
+      "Points": 6,
+      "Rebounds": 12,
+      "assists": 12,
+      "Steals": 22,
+      "Blocks": 5,
+      "SlamDunks": 12,
+    }
+  }
+ }
+};
+
+console.log(gameObject["home"]["players"]["Alan Anderson"]);
